4a0c598228f9d10261224ad47db127d386700e22
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-10-15 23:28:04 +0300

    add reverse for urls

--- src/eccormerce/urls.py
+++ src/eccormerce/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     url(r'^register/$', register_page),
     # url(r'^featured/$', ProductFeaturedListView.as_view()),
     # url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
-    url(r'^products/', include("products.urls")),
+    url(r'^products/', include("products.urls", namespace='products')),
     # url(r'^products-fbv/$', product_list_view),
     # url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
     #  url(r'^products/(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
--- src/products/models.py
+++ src/products/models.py
@@ -2,6 +2,7 @@ import random
 import os
 from django.db import models
 from django.db.models.signals import pre_save, post_save
+from django.urls import reverse
 
 
 from .utils import unique_slug_generator
@@ -59,8 +60,10 @@ class Product(models.Model):
 
     objects = ProductManager()
     
+   
     def get_absolute_url(self):
-        return "/products/{slug}/".format(slug=self.slug)
+        #return "/products/{slug}/".format(slug=self.slug)
+        return reverse("products:detail", kwargs={"slug": self.slug})
     
     
 
