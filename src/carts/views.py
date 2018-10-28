3af6ada70b6f582bcc146fdc5307f5d63a21453c
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-10-28 14:22:10 +0300

    checkout view

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 
+from orders.models import Order
 from products.models import Product
 from .models import Cart
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    return render(request, "carts/home.html", {"cart": cart_obj})
+    return render(request, "carts/home.html", {"carts": cart_obj})
 
 
 def cart_update(request):
@@ -15,7 +16,7 @@ def cart_update(request):
             product_obj = Product.objects.get(id=product_id)
         except Product.DoesNotExist:
             print("Show message to user, product is gone?")
-            return redirect("cart:home")
+            return redirect("carts:home")
         cart_obj, new_obj = Cart.objects.new_or_get(request)
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
@@ -24,3 +25,21 @@ def cart_update(request):
         request.session['cart_items'] = cart_obj.products.count()
         # return redirect(product_obj.get_absolute_url())
     return redirect("carts:home")
+
+
+
+def checkout_home(request):
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
+    order_obj = None
+    if cart_created or cart_obj.products.count() == 0:
+        return redirect("carts:home")
+    else:
+        order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+    return render(request, "carts/checkout.html", {"object": order_obj})
+
+
+
+
+
+
+
